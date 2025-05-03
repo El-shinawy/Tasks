aabcd14d29f531a36cefb0286742456314c5827b
Mohamed Ebrahim <mohamedelshinawy14@gmail.com>
2025-05-03 16:21:12 +0300

    Update app.js

--- EmployeeCrud/employee-crud/app.js
+++ EmployeeCrud/employee-crud/app.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/UserDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/UserDB');
 
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
