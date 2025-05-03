e824009509222adf51d72e9f9d0d3335b3fab70d
Mohamed Ebrahim <mohamedelshinawy14@gmail.com>
2025-05-03 16:22:04 +0300

    Update employeeRoutes.js

--- EmployeeCrud/employee-crud/routes/employeeRoutes.js
+++ EmployeeCrud/employee-crud/routes/employeeRoutes.js
@@ -2,38 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Employee = require('../models/Employee');
 
-// 🟢 إظهار كل الموظفين
 router.get('/', async (req, res) => {
   const employees = await Employee.find({});
   res.render('index', { employees });
 });
 
-// 🟡 فورم إضافة موظف جديد
 router.get('/new', (req, res) => {
   res.render('form', { employee: {}, action: '/new', method: 'POST' });
 });
 
-// 🟢 إنشاء موظف جديد
 router.post('/new', async (req, res) => {
   const { name, address, salary, gender } = req.body;
   await Employee.create({ name, address, salary, gender });
   res.redirect('/');
 });
 
-// 🟠 فورم تعديل موظف
 router.get('/edit/:id', async (req, res) => {
   const employee = await Employee.findById(req.params.id);
   res.render('form', { employee, action: `/edit/${employee._id}?_method=PUT`, method: 'POST' });
 });
 
-// 🔵 تعديل الموظف
 router.put('/edit/:id', async (req, res) => {
   const { name, address, salary, gender } = req.body;
   await Employee.findByIdAndUpdate(req.params.id, { name, address, salary, gender });
   res.redirect('/');
 });
 
-// 🔴 حذف موظف
 router.delete('/delete/:id', async (req, res) => {
   await Employee.findByIdAndDelete(req.params.id);
   res.redirect('/');
